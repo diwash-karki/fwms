6e91d7fcc030295938af893ccd92d84b5cc0a500
diwash-karki <diwashkarki7338@gmail.com>
2023-03-08 09:42:24 -0600

    manish fwms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Heading from './Heading';
+import Navigation from './Navigation';
+import Request from './Request';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-content'>
+      <Navigation />
+      <div className='right-view'>
+        <Heading />
+        <div className='body-wrapper'>
+          <Request />
+          <Request />
+          <Request />
+          <Request />
+        </div>
+
+      </div>
     </div>
   );
 }
